4c10e2fb67153328ac583ce2928a4d9e1bd42b07
sp simon <cmoxsh@gmail.com>
2021-04-03 03:30:47 +0600

    authentication social media signIn and SignUp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+// Site link: https://auth-repack.firebaseapp.com/
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 function App() {
+  const [users, setUsers] = useState({});
+  const { displayName, email, photoURL } = users;
+
+  // Google Sign In Func
+  // -----------------------
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    firebase.auth().signInWithPopup(googleProvider)
+      .then((result) => {
+        const credential = result.credential;
+        const accessToken = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        // console.log('Sign In Google User Props', user);
+        setUsers(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // ...
+      });
+  }
+
+
+  // Facebook Sign In Func
+  // --------------------------
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+  const handleFacebookSignIn = () => {
+    firebase.auth().signInWithPopup(facebookProvider)
+      .then((result) => {
+        const credential = result.credential;
+        const accessToken = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        // console.log('Sign In Facebook User Props', user);
+        setUsers(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ...
+      });
+  }
+
+
+  // Github Sign In Func
+  // -------------------------
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  const handleGithubSignIn = () => {
+    firebase.auth().signInWithPopup(githubProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+
+        // The signed-in user info.
+        var user = result.user;
+        console.log('Sign In Github User Props', user);
+        setUsers(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Firebase Authentication</h1>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+      <button onClick={handleGithubSignIn}>Github Sign In</button>
+      <div>
+        <img src={photoURL} alt="" style={{ marginTop: '10px' }} />
+        <h3>Name: {displayName}</h3>
+        <h4>Email: {email}</h4>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBy81JCfMEmMwMVoEOyKKUqr-8hKybbIZ4",
+    authDomain: "auth-repack.firebaseapp.com",
+    projectId: "auth-repack",
+    storageBucket: "auth-repack.appspot.com",
+    messagingSenderId: "864963899448",
+    appId: "1:864963899448:web:e82a88979e60dfae9976cd"
+  };
+
+  export default firebaseConfig;
